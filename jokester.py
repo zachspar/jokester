d6cfc7ff64d712bbddcbd7972163dcdfa59f7f8c
Zachary Spar <zachspar@gmail.com>
2019-09-24 21:35:39 -0700

    adding skeleton of hardware concurrency

--- jokester.py
+++ jokester.py
@@ -1,6 +1,7 @@
 import time
 from squid import *
 from button import *
+from threading import Thread
 from requests import get, RequestException
 
 
@@ -9,13 +10,16 @@ class Jokester(object):
         self._b = Button(25)
         self._led = Squid(18, 23, 24)
         self._led.set_color(GREEN)
-        
+        Thread(target=self._listen_for_press).start()
+
+    def _listen_for_press(self):
+        while True:
+            if self._b.is_pressed():
+                print("button pressed heard from thread!!")
+
     def main_loop(self):
         while True:
             if self._b.is_pressed():
-                self._led.set_color(PURPLE)
-                time.sleep(5)
-                self._led.set_color(GREEN)
                 self.get_rand_joke()
 
     def get_rand_joke(self):
